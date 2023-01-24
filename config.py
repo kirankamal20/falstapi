c7876fcef78e1a97471a025869c2e8ebb15b0836
kirankamal <kiran.k@lucidplus.com>
2023-01-24 18:14:27 +0530

    Codes are updated

--- config.py
+++ config.py
@@ -1,8 +1,36 @@
-import motor.motor_asyncio
+ 
 
-MONGODB_URL = 'mongodb://localhost:27017'
 
-client =  motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 
-# connect to database python_db
-database = client.python_db
+
+
+
+# import motor.motor_asyncio
+import pymongo
+# MONGODB_URL = 'mongodb+srv://Kiran123:<Kiran123>@cluster0.tqocdec.mongodb.net/?retryWrites=true&w=majority'
+
+# client =  motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
+
+# # connect to database python_db
+# database = client.test
+
+
+
+client = pymongo.MongoClient("mongodb+srv://Kiran123:Kiran123@cluster0.tqocdec.mongodb.net/?retryWrites=true&w=majority")
+database = client["test"]
+# # print results
+# for i in result:
+#     print(i)
+ 
+# from urllib.parse import quote_plus
+# username = quote_plus('<Kiran123>')
+# password = quote_plus('<Kiran123>')
+# cluster = '<Cluster0>'
+# authSource = '<authSource>'
+# authMechanism = '<authMechanism>'
+# uri = 'mongodb+srv://' + username + ':' + password + '@' + cluster + '/?authSource=' + authSource + '&authMechanism=' + authMechanism
+# client = pymongo.MongoClient(uri)
+# result = client["<dbName"]["<collName>"].find()
+# # print results
+# for i in result:
+#     print(i)
--- repositroy.py
+++ repositroy.py
@@ -8,8 +8,8 @@ class BookRepo():
     @staticmethod
     async def retrieve():
         _book = []
-        collection = database.get_collection('book').find()
-        async for book in collection:
+        collection = database['book'].find()
+        for book in collection:
             _book.append(book)
         return _book
 
@@ -21,19 +21,20 @@ class BookRepo():
             "title": book.title,
             "description": book.description,
         }
-        await database.get_collection('book').insert_one(_book)
+        database.get_collection('book').insert_one(_book)
 
     @staticmethod
     async def update(id: str, book: Book):
-        _book = await database.get_collection('book').find_one({"_id": id})
+        _book = database.get_collection('book').find_one({"_id": id})
         _book["title"] = book.title
         _book["description"] = book.description
-        await database.get_collection('book').update_one({"_id": id}, {"$set": _book})
+        database.get_collection('book').update_one(
+            {"_id": id}, {"$set": _book})
 
     @staticmethod
     async def retrieve_id(id: str):
-        return await database.get_collection('book').find_one({"_id": id})
+        return database.get_collection('book').find_one({"_id": id})
 
     @staticmethod
     async def delete(id: str):
-        await database.get_collection('book').delete_one({"_id": id})
+        database.get_collection('book').delete_one({"_id": id})
