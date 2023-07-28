225fdc71c157b1fabd4632217519f666a6a4be3f
kirankamal <kiran.k@lucidplus.com>
2023-07-28 10:36:34 +0530

    added login page

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from requests import request
 import router
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 #define endpoint
 @app.get("/")
-def home():
-    return "Welcome Home"
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    extensions = request.query_params.get("extensions", {})
+    return templates.TemplateResponse("index.html", {"request": request, "extensions": extensions})
 
 app.include_router(router.router)
